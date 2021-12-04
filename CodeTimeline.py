5c5521cb31a7d1ac4075f23910b8a84ea0e85b3c
Brian A. Danielak <briandaniela.k+github@gmail.com>
2021-12-04 16:49:14 -0500

    Begin rewrite of CodeTimeline2

--- CodeTimeline.py
+++ CodeTimeline.py
@@ -1,22 +1,36 @@
 import click
 import os
 
+from typing import Union
+from git import Repo
+
 
 @click.command()
-def code_timeline():
-    git_path = get_nearest_git_repo(os.getcwd())
+@click.option("--input", help="The file you'd like to see a timeline of")
+@click.option(
+    "--output", default="timeline.html", help="An optional name for the output file"
+)
+def code_timeline(input: str, output: str) -> None:
+    git_path = get_nearest_git_repo(input)
+    repository = Repo(git_path)
+    if repository.is_dirty():
+        click.echo(
+            "It looks like some files in this repo haven't had their changes committed. Please get the repository into a clean state before running CodeTimeline"
+        )
+        exit
     click.echo(f"git directory is {git_path}")
+    click.echo(repository)
 
 
-def get_nearest_git_repo(directory):
+def get_nearest_git_repo(directory: str) -> Union[str, None]:
     click.echo(f"current directory is {directory}")
     click.echo(f"is .git a subdirectory? {os.path.isdir('.git')}")
 
     if os.path.isdir(os.path.join(directory, ".git")) is True:
         return directory
+
     head, tail = os.path.split(directory)
-    click.echo(f"head is {head}")
-    if tail is not "":
+    if tail != "":
         return get_nearest_git_repo(head)
     else:
         return None
